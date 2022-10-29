1956976c38a0b5eb70732332e6935ea019580968
inix84 <inix84@mail.ru>
2022-10-29 17:40:51 +0500

    Задание 3 // как использовать тут метод split() непоняла :(

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
         String fullName = lastName+" "+firstName+" "+middleName;
         System.out.println("ФИО сотрудника — "+fullName);
         String fullName1 = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName1);
+        boolean OutSymbol=fullName.contains("ё");
+        if (OutSymbol=true) {
+            System.out.print("Будет произведена замена буквы Ё на Е: ");
+            String fullName2 = fullName.replace('ё','е');
+            System.out.println("Данные ФИО сотрудника — "+fullName2);
+        }
+
 
     }
 }
